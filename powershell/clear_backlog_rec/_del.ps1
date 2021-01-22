55b036ebd741db8050ec933e9acad1cc0e149937
Onyx and Iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-01-22 12:16:41 +0000

    Added _del.ps1
    removes recordings older than 14 days from RECORDINGS directory recursively. Excludes any files in _saved/* and writes activity to log file. Will accept two switch parameters to alter the function of the code. If no files found will also write to file.

---
+++ powershell/clear_backlog_rec/_del.ps1
@@ -0,0 +1,37 @@
+# This script monitors the folder we save all of our recordings to
+# and deletes mp4 and mkv files older than two weeks.
+# excludes and logs altered (according to code) with parameters
+param([switch]$dualstreams,[switch]$reviews)
+
+$path_rec = "PATH\TO\RECORDINGS"
+$backlog = (Get-Date).AddDays(-14)
+$filetypes = ('*.mp4', '*.mkv')  
+
+$logfile = "E:\scripts\cronjobs\monitor_streamvids\logfile.txt"
+
+
+# set excludes and modify log based on parameters
+if ( $PSBoundParameters.Values.Count -eq 0 ) {
+  $excludes = "_saved"
+  $write_tolog = 'Add-Content -Path $logfile -Value "No recordings older than 14 days"'
+} elseif ( $dualstreams ) {
+  $excludes = "_saved|reviews"
+  $write_tolog = 'Add-Content -Path $logfile -Value "No dualstreams older than 14 days"'
+} elseif ( $reviews ) {
+  $excludes = "_saved|dualstreams"
+  $write_tolog = 'Add-Content -Path $logfile -Value "No reviews older than 14 days"'
+}
+
+# bash-like logical OR does not work with invoke-expression, will always return 0
+# since the expression was invoked succesfully whether the command failed or not.
+# Invoke-Expression $cleanup; if (-not $?) { Invoke-Expression $write_tolog }
+
+$count = 0
+ForEach ($file in $(Get-ChildItem -Recurse $path_rec -Include $filetypes -File | Where-Object {$_.PSParentPath -notmatch $excludes -and $_.CreationTime -lt $backlog})) {
+  $count += $file.count
+  Remove-Item $file -Verbose 4>> $logfile 
+}; if (-not $count) { Invoke-Expression $write_tolog } else {
+  
+  "$count files cleared from backlog" | Add-Content $logfile
+
+exit
