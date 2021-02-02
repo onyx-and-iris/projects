d08823ed88f925049f3495071613791fb306968f
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-02-02 22:56:03 +0000

    added world_runners 3
    same program as world runners but in python.
    
    Uses a nested dictionary to store values for each athlete.
    
    Script will prompt for data input until told not to.
    
    Data from file is retrieved before adding new values.
    
    Function reset will truncate the db to 0 bytes.

---
+++ python/world_runners_III/runners.py
@@ -0,0 +1,96 @@
+import pickle
+import argparse
+import sys
+from pathlib import Path
+
+class fileOps:
+    def __init__(self):
+        self.db = {}
+        self.file_db = "records.db"
+
+    def createEmpty(self):
+        Path(self.file_db).touch()
+
+    def writeFile(self):
+        records = open(self.file_db, "wb")
+
+        pickle.dump(self.db, records)
+        records.close
+
+    def readFile(self):
+        # attempt read from file if not exist then create file
+        try:
+            with open(self.file_db, "rb") as records:
+                self.db = pickle.load(records)
+                records.close
+
+        except EOFError:
+            return self.db
+        except IOError:
+            self.createEmpty()
+
+        return self.db
+
+    def truncate(self):
+        records = open(self.file_db, "wb")
+        records.truncate(0)
+        records.close
+
+
+class records:
+    def newRecord():
+        fileIO = fileOps()
+        db = fileIO.readFile()
+        next = 'y'
+
+        while next == 'y':
+            name = input('Please enter athlete short name: ')
+            db[name] = {}
+            name_full = input('Please enter athlete full name: ')
+            db[name]['name'] = name_full
+            age = input('Age:')
+            db[name]['age'] = age
+            country = input('Country:')
+            db[name]['country'] = country
+            event = input('Event:')
+            db[name]['event'] = event
+            pb = input('PB:')
+            db[name]['pb'] = pb
+
+            next = input('Add another record (y/n)? ')
+
+        fileIO.writeFile()
+
+        
+    def getRecords():
+        fileIO = fileOps()
+        db = fileIO.readFile()
+
+        if not db:
+            print("No records set yet, please set some!")
+        else:
+            for key in db:
+                print(f"{db[key]['name']} \n==============\n"
+                f"{db[key]['age']}\n{db[key]['country']}\n"
+                f"{db[key]['event']}\n{db[key]['pb']}\n")
+
+    def reset():
+        fileIO = fileOps()
+        response = input("Truncating database, are you sure? (y/n) ")
+        if response == "y":
+            fileIO.truncate()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', action='store_true')
+    parser.add_argument('-g', action='store_true')
+    parser.add_argument('-t', action='store_true')
+    args = parser.parse_args()
+
+    if args.n:
+        records.newRecord()
+    elif args.g:
+        records.getRecords()
+    elif args.t:
+        records.reset()
