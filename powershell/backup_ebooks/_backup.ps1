4e8b308406314e1a69c0ffcae592daa4953c5ac8
Onyx and Iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-02-02 15:38:51 +0000

    updated ebook backup
    Added some error handling in case destination list is empty.
    
    Also creates log file if it doesn't exist.
    
    Using Tee-Object to write to console and log file simultaenously.

--- powershell/backup_ebooks/_backup.ps1
+++ powershell/backup_ebooks/_backup.ps1
@@ -1,17 +1,46 @@
-$d = Read-Host -Prompt 'Drive letter of usb flashpen?'
+# Script to backup ebooks to usb drive. Script will prompt for destination drive letter
+# Creates a comparison list to ensure only ebooks not previously backed up are copied.
+# Error handling in case destination is empty will simply copy all available ebooks to
+# flash pen. Script assumes it's in your ebooks directory otherwise set path of source.
+# Script ignores any files in a folder named 'My Kindle Content' (duplicates).
 
-$source = "PATH\TO\ebooks"
-$destination = "${d}:\ebooks"
+# Feel free to use this script or modify it for your own purposes. I take no responsibility
+# for use of this script and I don't make any promises that it will be suitable for any
+# specific purpose. I find it useful for my own needs.
+
+$usb_drive = Read-Host -Prompt 'Drive letter of usb flashpen?'
+
+$source = "$PWD"
+$destination = "${usb_drive}:\ebooks"
+
+$logfile = "$PWD\_backup.log"
+
+# check if logfile exists, if not create.
+If (-Not [System.IO.File]::Exists($path)) {
+	Out-File -FilePath $logfile
+}
 
 # full list of files in source and destination
 $list_s = Get-ChildItem -Recurse -path $source
 $list_d = Get-ChildItem -Recurse -path $destination
 
-# difference list
-Write-Host "Copying only files that don't exist in destination:"
+Write-Host $list_s
+Write-Host $list_d
 
-ForEach ($file in (Compare-Object -ReferenceObject $list_s -DifferenceObject $list_d -PassThru | `
-Where-Object { $_.PSParentPath -notlike "*My Kindle Content" -and $_.Extension -match ".pdf|.epub|.zip"})) {
-        Write-Host "$file to $destination"
-        Copy-Item $source\$file $destination
-    }
+# difference list
+Try {
+	ForEach ($file in (Compare-Object -ReferenceObject $list_s -DifferenceObject $list_d -PassThru | `
+	Where-Object { $_.PSParentPath -notlike "*My Kindle Content" -and $_.Extension -match ".pdf|.epub|.zip"})) {
+			"Copying only files that don't exist in destination:" | Tee-Object -Append $logfile
+			"$file to $destination" | Tee-Object -Append $logfile
+			Copy-Item $source\$file $destination
+		}
+} Catch {
+	Write-Host "Either source or destination are empty, failed to generate comparison list"
+ 
+	"Copying ebooks to destination!" | Tee-Object -Append $logfile
+	
+	Get-ChildItem -Recurse -path $source | `
+	Where-Object { $_.PSParentPath -notlike "*My Kindle Content" -and $_.Extension -match ".pdf|.epub|.zip"} | `
+	Copy-Item -Destination $destination -Recurse -Force -verbose | Tee-Object -Append $logfile
+}
