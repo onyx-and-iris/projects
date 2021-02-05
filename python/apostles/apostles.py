e9ad9ad8401347320f1ce873995ab88567b911a5
onyx <onyx@onxyandiris.online>
2021-02-05 12:34:46 +0000

    moved html variables into class Html for apostles.py. Removed from server.py

--- python/apostles/apostles.py
+++ python/apostles/apostles.py
@@ -5,8 +5,35 @@ from bs4 import BeautifulSoup
 import argparse
 
 class parseHtml:
-    def __init__(self, soup):
+    def __init__(self, soup=None):
         self.soup = soup    
+
+        self.url_kj = {
+            'matthew': "http://www.earlychristianwritings.com/text/matthew-kjv.html",
+            'mark': "http://www.earlychristianwritings.com/text/mark-kjv.html",
+            'luke': "http://www.earlychristianwritings.com/text/luke-kjv.html",
+            'john': "http://www.earlychristianwritings.com/text/john-kjv.html"
+        }
+
+        self.url_as = {
+            'matthew': "http://www.earlychristianwritings.com/text/matthew-asv.html",
+            'mark': "http://www.earlychristianwritings.com/text/mark-asv.html",
+            'luke': "http://www.earlychristianwritings.com/text/luke-asv.html",
+            'john': "http://www.earlychristianwritings.com/text/john-asv.html"
+        }
+
+        self.url_we = {
+            'matthew': "http://www.earlychristianwritings.com/text/matthew-web.html",
+            'mark': "http://www.earlychristianwritings.com/text/mark-web.html",
+            'luke': "http://www.earlychristianwritings.com/text/luke-web.html",
+            'john': "http://www.earlychristianwritings.com/text/john-web.html"        
+        }
+        
+        self.version = {}
+        self.version['king_james'] = self.url_kj
+        self.version['american_standard'] = self.url_as
+        self.version['world_english'] = self.url_we
+
 
     def king_james(self): 
         # remove all javascript and stylesheet code
@@ -48,37 +75,14 @@ if __name__ == '__main__':
     parser.add_argument('-b')
     args = parser.parse_args()
 
-    url_kj = {
-        'matthew': "http://www.earlychristianwritings.com/text/matthew-kjv.html",
-        'mark': "http://www.earlychristianwritings.com/text/mark-kjv.html",
-        'luke': "http://www.earlychristianwritings.com/text/luke-kjv.html",
-        'john': "http://www.earlychristianwritings.com/text/john-kjv.html"
-    }
-
-    url_as = {
-        'matthew': "http://www.earlychristianwritings.com/text/matthew-asv.html",
-        'mark': "http://www.earlychristianwritings.com/text/mark-asv.html",
-        'luke': "http://www.earlychristianwritings.com/text/luke-asv.html",
-        'john': "http://www.earlychristianwritings.com/text/john-asv.html"
-    }
-
-    url_we = {
-        'matthew': "http://www.earlychristianwritings.com/text/matthew-web.html",
-        'mark': "http://www.earlychristianwritings.com/text/mark-web.html",
-        'luke': "http://www.earlychristianwritings.com/text/luke-web.html",
-        'john': "http://www.earlychristianwritings.com/text/john-web.html"        
-    }
-    
-    version = {}
-    version['king_james'] = url_kj
-    version['american_standard'] = url_as
-    version['world_english'] = url_we
-
     html_text = None
+    soup = None
 
-    if args.v in version:
-        if args.b in version[args.v]:
-            html_text = requests.get(version[args.v][args.b]).text
+    book = parseHtml(soup)
+
+    if args.v in book.version:
+        if args.b in book.version[args.v]:
+            html_text = requests.get(book.version[args.v][args.b]).text
         else:
             print(f'No such book, available options:\n'
             f'matthew\nmark\nluke\njohn\n')            
@@ -94,5 +98,5 @@ if __name__ == '__main__':
         parse_bymethod = getattr(book, args.v)
         text = parse_bymethod() 
 
-        fileIO = fileops()
+        fileIO = fileOps()
         fileIO.writeTofile(text)
--- python/apostles/server.py
+++ python/apostles/server.py
@@ -39,37 +39,14 @@ if __name__ == '__main__':
 
     args = runServer.run()
 
-    url_kj = {
-        'matthew': "http://www.earlychristianwritings.com/text/matthew-kjv.html",
-        'mark': "http://www.earlychristianwritings.com/text/mark-kjv.html",
-        'luke': "http://www.earlychristianwritings.com/text/luke-kjv.html",
-        'john': "http://www.earlychristianwritings.com/text/john-kjv.html"
-    }
-
-    url_as = {
-        'matthew': "http://www.earlychristianwritings.com/text/matthew-asv.html",
-        'mark': "http://www.earlychristianwritings.com/text/mark-asv.html",
-        'luke': "http://www.earlychristianwritings.com/text/luke-asv.html",
-        'john': "http://www.earlychristianwritings.com/text/john-asv.html"
-    }
-
-    url_we = {
-        'matthew': "http://www.earlychristianwritings.com/text/matthew-web.html",
-        'mark': "http://www.earlychristianwritings.com/text/mark-web.html",
-        'luke': "http://www.earlychristianwritings.com/text/luke-web.html",
-        'john': "http://www.earlychristianwritings.com/text/john-web.html"        
-    }
-                    
-    version = {}
-    version['king_james'] = url_kj
-    version['american_standard'] = url_as
-    version['world_english'] = url_we
-
+    soup = None
     html_text = None
 
-    if args[0] in version:
-        if args[1] in version[args[0]]:
-            html_text = requests.get(version[args[0]][args[1]]).text
+    book = apostles.parseHtml(soup)
+
+    if args[0] in book.version:
+        if args[1] in book.version[args[0]]:
+            html_text = requests.get(book.version[args[0]][args[1]]).text
         else:
             print(f'No such book available!')
     else:
@@ -80,6 +57,7 @@ if __name__ == '__main__':
 
         # use the appropriate parser
         book = apostles.parseHtml(soup)
+
         parse_bymethod = getattr(book, args[0])
         text = parse_bymethod() 
 
