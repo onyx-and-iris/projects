53de198ebbf72fb1bc9c4a321ecdd7726b5ca379
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-02-05 17:14:28 +0000

    update gitignore, update apostles client.py
    ignore __pycache__ folders
    
    added locals class which backups local retrieved text and checks if exists already

--- python/apostles/client.py
+++ python/apostles/client.py
@@ -1,15 +1,20 @@
 import argparse
 import socket
+import os
+
+class locals:
+    def __init__(self, version, book):
+        self.thisBook = version + '_' + book + '.txt'
+
+    def has_book(self):
+        return os.path.isfile(self.thisBook)
+
+    def backup_book(self):
+        if os.path.isfile('book.txt'):
+            os.rename('book.txt', self.thisBook)
 
 class retrieve_text:
     def __enter__(self):
-        return self
-
-    def __init__(self):
-        # mapped in local hosts file
-        self.HOST = 'x.x.x.x'
-        self.PORT = 60000
-
         try:
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error:
@@ -18,6 +23,13 @@ class retrieve_text:
         print('Socket Created')
 
         self.s.connect((self.HOST,self.PORT))
+
+        return self
+
+    def __init__(self):
+        # mapped in local hosts file
+        self.HOST = 'vps.onyxandiris.online'
+        self.PORT = 60000
 
     def send_req(self, request):
         self.s.send(request.encode())
@@ -59,11 +71,19 @@ if __name__ == '__main__':
     else:
         print(f'No such version, available options:\n'
         f'king_james\namerican_standard\nworld_english\n')
-    
-    with retrieve_text() as retrieve:
-        if request:
-            retrieve.send_req(request)
-        else:
-            print(f'Failed to generate request string... exiting')
 
-        retrieve.wait_reply()
+    # check if we already have book
+    check_lib = locals(args.v, args.b)
+
+    if check_lib.has_book():
+        print('Book already in local storage')
+    else:
+        with retrieve_text() as retrieve:
+            if request:
+                retrieve.send_req(request)
+            else:
+                print(f'Failed to generate request string... exiting')
+
+            retrieve.wait_reply()
+
+            check_lib.backup_book()
