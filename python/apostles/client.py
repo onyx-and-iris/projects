20fd17ee51d6a4e88a1e857bed3e4fd984e00441
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-02-05 02:21:05 +0000

    update to client.py
    remove the nested dictionary, not needed in this script.
    
    arguments checked against  tuples instead.
    
    moved socket create send and close into function send_req

--- python/apostles/client.py
+++ python/apostles/client.py
@@ -1,61 +1,46 @@
 import argparse
 import socket
 
-# mapped in local hosts file
-HOST = 'x.x.x.x'
-PORT = 60000
+def send_req(request):
+    # mapped in local hosts file
+    HOST = 'X.X.X.X'
+    PORT = 60000
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-    print('Failed to create socket')
-    sys.exit()
-print('Socket Created')
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print('Failed to create socket')
+        sys.exit()
+    print('Socket Created')
 
-s.connect((HOST,PORT))
+    s.connect((HOST,PORT))
+    
+    s.send(request.encode())
+    
+    s.close
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-v')
-parser.add_argument('-b')
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v')
+    parser.add_argument('-b')
+    args = parser.parse_args()
 
-url_kj = {
-    'matthew': "http://www.earlychristianwritings.com/text/matthew-kjv.html",
-    'mark': "http://www.earlychristianwritings.com/text/mark-kjv.html",
-    'luke': "http://www.earlychristianwritings.com/text/luke-kjv.html",
-    'john': "http://www.earlychristianwritings.com/text/john-kjv.html"
-}
+    version = ('king_james', 'american_standard', 'world_english')
+    book = ('matthew', 'mark', 'luke', 'john')
 
-url_as = {
-    'matthew': "http://www.earlychristianwritings.com/text/matthew-asv.html",
-    'mark': "http://www.earlychristianwritings.com/text/mark-asv.html",
-    'luke': "http://www.earlychristianwritings.com/text/luke-asv.html",
-    'john': "http://www.earlychristianwritings.com/text/john-asv.html"
-}
+    request = None
 
-url_we = {
-    'matthew': "http://www.earlychristianwritings.com/text/matthew-web.html",
-    'mark': "http://www.earlychristianwritings.com/text/mark-web.html",
-    'luke': "http://www.earlychristianwritings.com/text/luke-web.html",
-    'john': "http://www.earlychristianwritings.com/text/john-web.html"        
-}
-
-version = {}
-version['king_james'] = url_kj
-version['american_standard'] = url_as
-version['world_english'] = url_we
-
-html_text = None
-
-if args.v in version:
-    if args.b in version[args.v]:
-        string = args.v + " " + args.b
-        s.send(string.encode())
+    if args.v in version:
+        if args.b in book:
+            request = args.v + " " + args.b
+        else:
+            print(f'No such book, available options:\n'
+            f'matthew\nmark\nluke\njohn\n')            
     else:
-        print(f'No such book, available options:\n'
-        f'matthew\nmark\nluke\njohn\n')            
-else:
-    print(f'No such version, available options:\n'
-    f'king_james\namerican_standard\nworld_english\n')
-
-s.close
+        print(f'No such version, available options:\n'
+        f'king_james\namerican_standard\nworld_english\n')
+    
+    if request:
+        send_req(request)
+    else:
+        print(f'Failed to generate request string... exiting')
