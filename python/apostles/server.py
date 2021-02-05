ec8dcfa639382842ad37a731375e745e7ce378ee
onyx <onyx@onxyandiris.online>
2021-02-05 15:52:47 +0000

    added function serve, will send back text to client. Server class now invoked using with statement

--- python/apostles/server.py
+++ python/apostles/server.py
@@ -3,66 +3,80 @@ import requests
 from bs4 import BeautifulSoup
 import socket
 
-class listen:
+class server:
+    def __enter__(self):
+        return self
+
     def __init__(self):
         self.HOST = ''
         self.PORT = 60000
 
-    def run(self):
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error:
             print('Failed to create socket') 
             sys.exit()
         print('Socket Created') 
 
-        s.bind ((self.HOST,self.PORT))
-        s.listen()
+        self.s.bind ((self.HOST,self.PORT))
+        self.s.listen()
+        self.conn, self.addr = self.s.accept()
 
-        conn, addr = s.accept()
-
+    def run(self):
         while True:
-            req = conn.recv(1024).decode('utf-8')
-            print(req)
+            req = self.conn.recv(1024).decode('utf-8')
             if req:
                 args = list(req.split())
-                print(f"client has requested version:{args[0]}\n"
-                f"and book:{args[1]}\n")
-
-                s.close()
+                print(f'client has requested version:{args[0]}\n'
+                f'and book:{args[1]}\n')
                 
+                response = 'request received, getting your book...'
+                self.conn.send(response.encode())
                 break
+
         return args
 
-if __name__ == '__main__':
-    runServer = listen()
+    def serve(self):
+        with open("book.txt", "r") as data:
+            for line in data:
+                self.conn.send(line.encode())
 
-    args = runServer.run()
-
-    soup = None
-    html_text = None
-
-    book = apostles.parseHtml(soup)
-
-    if args[0] in book.version:
-        if args[1] in book.version[args[0]]:
-            html_text = requests.get(book.version[args[0]][args[1]]).text
-        else:
-            print(f'No such book available!')
-    else:
-        print(f'No such version available!')
-
-    if html_text:
-        soup = BeautifulSoup(html_text, 'html.parser')
-
-        # use the appropriate parser
-        book = apostles.parseHtml(soup)
-
-        parse_bymethod = getattr(book, args[0])
-        text = parse_bymethod() 
-
-        fileIO = apostles.fileOps()
-        fileIO.writeTofile(text)
+    def __exit__(self, type, value, traceback):
+        self.s.shutdown(socket.SHUT_RDWR)
+        self.s.close()
+        print('Socket Closed')
         
 
+
+if __name__ == '__main__':
+    with server() as serve_text:
+        args = serve_text.run()
+
+        soup = None
+        html_text = None
+
+        book = apostles.parseHtml(soup)
+
+        if args[0] in book.version:
+            if args[1] in book.version[args[0]]:
+                html_text = requests.get(book.version[args[0]][args[1]]).text
+            else:
+                print(f'No such book available!')
+        else:
+            print(f'No such version available!')
+
+        if html_text:
+            soup = BeautifulSoup(html_text, 'html.parser')
+
+            # use the appropriate parser
+            book = apostles.parseHtml(soup)
+            parse_bymethod = getattr(book, args[0])
+            text = parse_bymethod() 
+
+            # write text to file on server
+            fileIO = apostles.fileOps()
+            fileIO.writeTofile(text)
+
+            # server data to client
+            serve_text.serve()
 
