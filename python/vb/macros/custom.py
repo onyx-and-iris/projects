db7d5fd7e7146ec550e091ebe05c0f44996c8fef
norm <stream@onyxandiris.online>
2021-02-08 04:33:31 +0000

    Update custom.py
    cleared up some of the variables names in button_setstate.
    
    framecounter removed form vban_sendtext and samplerate set to 0.

--- python/vb/macros/custom.py
+++ python/vb/macros/custom.py
@@ -2,43 +2,45 @@ import socket
 
 from ctypes import *
 from voicemeeter.driver import dll
-
+from sys import stderr
 
 class commands:
-  def button_setState(index, state):
-    """ long __stdcall VBVMR_MacroButton_SetStatus(long nuLogicalButton, float fValue, long bitmode); """
-    macro_setstatus = dll.VBVMR_MacroButton_SetStatus
-    c_l_logical = c_long(index)
-    # use this value to turn macro state on and off
-    c_f_val = c_float(state)
-    c_l_mode = c_long(2)
+  def button_setState(index, state, mode=2):
+    """ Direct call to VBVMR_MacroButton_SetStatus """
+    cbf_setstatus = dll.VBVMR_MacroButton_SetStatus
+    # set expected args and return type
+    cbf_setstatus.argtypes = [c_long, c_float, c_long]
+    cbf_setstatus.restype = c_long
 
-    retval = macro_setstatus(c_l_logical, c_f_val, c_l_mode)
+    c_index = c_long(index)
+    c_state = c_float(state)
+    c_mode = c_long(mode)
 
-    if not retval:
-      print('Call back function return: OK')
+    retval = cbf_setstatus(c_index, c_state, c_mode)
+    if retval:
+      print(f'ERROR: Callback button_setState logical ID: [{int(c_index.value)}]'
+      , file=stderr)
 
-  def VBAN_SendText(ip, text):
+  def VBAN_SendText(ip, text, port=6980, s_name='Command1'):
     """ Credits go to TheStaticTurtle https://github.com/TheStaticTurtle/pyVBAN """
     IP = ip
-    PORT = 6990
-    streamName = 'sound_t'
-    sampleRate = 0
+    PORT = port
+    S_NAME = s_name
+    if len(S_NAME) > 16:
+      print('ERROR streamname too long', file=stderr)
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     s.connect((IP, PORT))
-    VBAN_BPSList = [0, 110, 150, 300, 600, 1200, 2400, 4800, 9600, 14400,19200, 31250, 38400, 57600, 115200, 128000, 230400, 250000, 256000, 460800,921600, 1000000, 1500000, 2000000, 3000000]
-    framecounter = 0
 
-    header  = 'VBAN' 
-    header += chr(int('0b01000000',2)  + VBAN_BPSList.index(sampleRate))
-    header += chr(int('0b00000000',2))
-    header += chr(int('0b00000000',2))
+    header  = 'VBAN'
+    header += chr(int('0b01000000',2)) # SR = 0
+    header += chr(int('0b00000000',2)) # bit mode (must be 0)
+    header += chr(int('0b00000000',2)) # for subchannels 
     header += chr(int('0b00010000',2)) # UTF8
-    header += streamName + "\x00" * (16 - len(streamName))
-    header += str(framecounter) + "\x00" * (4 - len(str(framecounter)))
+    header += S_NAME + "\x00" * (16 - len(S_NAME))
+    header += "\x00" * 32 # framecounter
 
     try:
-      framecounter += 1
       rawData = header + text 
       s.sendto(rawData.encode(), (IP, PORT))
 
