2ed83cf6625ef5e9602ebccef4427b0c97a1abaf
norm <stream@onyxandiris.online>
2021-02-08 04:35:53 +0000

    Update layerTwo.py
    removed argparse not required in layerTwo.py for version 2 of macros
    
    added reset function
    
    cleared up verbose

--- python/vb/macros/layerTwo.py
+++ python/vb/macros/layerTwo.py
@@ -3,25 +3,23 @@
 # todo: layer 3
 
 import voicemeeter
-import argparse
 import keypress
 
 from custom import commands
 
 
-class updates():
+class updates:
   def __init__(self):
     """ map macro names to logical IDS """
     self.button_map = [
-    ('brb', 30), ('onyx_only', 31), ('iris_only', 32),
+    ('onyx_only', 31), ('iris_only', 32),
     ('dual_scene', 40), ('onyx_big', 41), ('iris_big', 42),
-    ('end', 50)
+    ('start', 20), ('brb', 30), ('end', 50), ('reset', 72)
     ]
 
   def Button_State(self, macro, upd):
     for macro_name, logical_id in self.button_map:
       if macro_name == macro:
-        print(f'Macro = {macro_name} Button ID = {logical_id} State = {upd}')
         commands.button_setState(logical_id, upd)
         
         break
@@ -29,35 +27,15 @@ class updates():
   def reset_Button_State(self, data_def):
     """ run lists from default macros dict through button_state function """
     for arg, data in data_def['layer2'].items():
-      print(f'{data[0]} {data[1]}')
       self.Button_State(data[0], data[1])
 
-class macros():
+class macros:
   def __init__(self, macro, switch):
     self.macro = macro
     self.switch = switch
     
     self.update = updates()
-    
-    print(f'Switch passed to function in layer2: {self.switch}')
-    
-  # mutes both game pcs and sets the scene to 'BRB'
-  def brb(self):
-    with voicemeeter.remote('potato') as oai:
-      oai.show()
-      
-      oai.apply({
-        'in-2': dict(mute=True),
-        'in-3': dict(mute=True)
-      })
-      print("BRB: mics are muted")
-
-    self.update.Button_State(self.macro, self.switch)
-    
-    with keypress.sendkey(self.macro) as oai_key:
-      oai_key.action()  
-
-    return self.switch
+    self.sendkey = keypress.sendkey(self.macro)
 
   # unmute onyx pc mute iris pc 
   # and sets the scene to 'onyx_only'
@@ -70,10 +48,9 @@ class macros():
         'in-3': dict(mute=True)
       })
       
-    self.update.Button_State(self.macro, self.switch)      
+      self.update.Button_State(self.macro, self.switch)      
 
-    with keypress.sendkey(self.macro) as oai_key:
-      oai_key.action()
+    self.sendkey.action()
       
     print("Only Onyx Scene enabled, Iris mic muted")
     
@@ -90,10 +67,9 @@ class macros():
         'in-3': dict(mute=False)
       })
 
-    self.update.Button_State(self.macro, self.switch)      
+      self.update.Button_State(self.macro, self.switch)      
 
-    with keypress.sendkey(self.macro) as oai_key:
-      oai_key.action()
+    self.sendkey.action()
       
     print("Only Iris Scene enabled, Iris mic muted")
     
@@ -110,10 +86,9 @@ class macros():
         'in-3': dict(A5=1, mute=False, gain=0)
       })    
       
-    self.update.Button_State(self.macro, self.switch)
+      self.update.Button_State(self.macro, self.switch)
     
-    with keypress.sendkey(self.macro) as oai_key:
-      oai_key.action()
+    self.sendkey.action()
       
     print("Daul Scene enabled")
     
@@ -129,11 +104,10 @@ class macros():
         'in-3': dict(mute=False, gain=-3),
       })    
       
-    self.update.Button_State(self.macro, self.switch)
+      self.update.Button_State(self.macro, self.switch)
 
-    with keypress.sendkey(self.macro) as oai_key:
-      oai_key.action()
-      
+    self.sendkey.action()
+
     print("Onyx Big scene enabled")
     
     return self.switch
@@ -148,15 +122,48 @@ class macros():
         'in-3': dict(A5=1, mute=False, gain=0)
       })    
       
-    self.update.Button_State(self.macro, self.switch)
+      self.update.Button_State(self.macro, self.switch)
 
-    with keypress.sendkey(self.macro) as oai_key:
-      oai_key.action()
+    self.sendkey.action()
       
     print("Iris Big enabled")
     
     return self.switch
     
+  # mute game pcs to stream for start scene
+  # perhaps add call to subprocess to notify when stream goes live
+  def start(self):
+    with voicemeeter.remote('potato') as oai:
+      oai.show()
+      
+      oai.inputs[2].mute = True
+      oai.inputs[3].mute = True
+
+      self.update.Button_State(self.macro, self.switch)    
+    
+    self.sendkey.action() 
+
+    print("Start scene enabled.. ready to go live!")
+    
+    return self.switch
+
+  # mutes both game pcs and sets the scene to 'BRB'
+  def brb(self):
+    with voicemeeter.remote('potato') as oai:
+      oai.show()
+      
+      oai.apply({
+        'in-2': dict(mute=True),
+        'in-3': dict(mute=True)
+      })
+      print("BRB: mics are muted")
+
+      self.update.Button_State(self.macro, self.switch)
+
+    self.sendkey.action()
+
+    return self.switch
+
   # mute both game pcs leave mics unmuted for byes
   def end(self):
     with voicemeeter.remote('potato') as oai:
@@ -165,11 +172,14 @@ class macros():
       oai.inputs[2].mute = True
       oai.inputs[3].mute = True
 
-    self.update.Button_State(self.macro, self.switch)    
+      self.update.Button_State(self.macro, self.switch)    
     
-    with keypress.sendkey(self.macro) as oai_key:
-      oai_key.action()  
+    self.sendkey.action()
 
     print("Start scene enabled.. ready to go live!")
     
     return self.switch
+
+  def reset(self, default_states):
+    with voicemeeter.remote('potato') as oai:
+      self.update.reset_Button_State(default_states)
