2c9390df7d8191c1c12c3f601c0f289c350ff776
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-02-11 15:33:34 +0000

    added main.
    added main for testing
    
    cache seems to be reading writing reliably across runs, need to setup a reset function and memory map of the strip bus layouts.
    
    need to add set/get params string

--- ruby/vb_remote/macros.rb
+++ ruby/vb_remote/macros.rb
@@ -20,35 +20,39 @@ module VMR
     [:long, :float, :long], :long
 
     
-    # set/get params
+    # set/get params float
     attach_function :set_paramF, :VBVMR_SetParameterFloat, [:string, :float], :int
     attach_function :get_paramF, :VBVMR_GetParameterFloat, [:string, :pointer], :int
     attach_function :param_isDirty, :VBVMR_IsParametersDirty, [], :int
 
+    # set/get params string
+
     @cache = Hash(nil)
     @config = "myconfig.toml"
+    @file_db = "cache.dat"
 
     class << self
         attr_accessor :cache
         attr_accessor :config
+        attr_accessor :file_db
 
         def _initialize_cache
             if File.file?(VMR.config)
                 puts "Loading from config file"
                 VMR.cache = (TOML.load_file(VMR.config))
                 puts VMR.cache
-            end
+            
             """ If no config set to base configuration """
+            elsif File.file?(VMR.file_db)
+                _load_cache
+                puts "initialize cache:\n#{VMR.cache}"
+            end
+            """ otherwise empty hash """
         end
-
-        def _save_config
-
-        end
-
 
         def _clear_datapipe
             """ Poll param_isDirty until pipe is clear """
-            while param_isDirty !=0 do
+            until param_isDirty ==0 do
             end
         end
 
@@ -62,9 +66,8 @@ module VMR
             if ret != 0
                 puts "ERROR: CBF failed: #{ret}"
             else
-                return c_get.get_long(0)
+                c_get.get_long(0)
             end
-            
         end
 
         def _login
@@ -96,7 +99,14 @@ module VMR
         end
 
         def _set_parameter(name, set)
-            """ VBVMR_SetParameterFloat """
+            """ if value in cache and same as set return early """
+            if VMR.cache.include? name
+                if VMR.cache[name] == set
+                    return nil
+                end
+            end
+
+            """ Otherwise... VBVMR_SetParameterFloat """
             c_get_old = FFI::MemoryPointer.new(:float, 1)
             c_get_new = FFI::MemoryPointer.new(:float, 1)
 
@@ -107,10 +117,10 @@ module VMR
                 set_paramF(name, set.to_f)
                 get_paramF(name, c_get_new)
                 newval = c_get_new.get_float(0)
-                  
-                break if oldval != newval
 
                 _clear_datapipe
+                  
+                break if oldval != newval
             end
 
             get_paramF(name, c_get_new)
@@ -144,7 +154,7 @@ module VMR
         end
 
         def _load_cache(name = nil, full = 0)
-            VMR.cache = Marshal.load(File.binread("cache.dat"))
+            VMR.cache = Marshal.load(File.binread(VMR.file_db))
 
             if name != nil
                 return VMR.cache["#{name}"]
@@ -183,79 +193,3 @@ module VMR
         end
     end
 end
-
-
-#########################################
-param = "Strip[1].mute"
-button_ID = 0
-
-VMR._login
-
-VMR._special_command('Show')
-
-puts "Setting button [#{button_ID}] to 1"
-puts "=================================="
-VMR._macro_status(button_ID, 1)
-
-sleep(1)
-
-puts "\nSetting button [#{button_ID}] to 0"
-puts "=================================="
-VMR._macro_status(button_ID, 0)
-
-sleep(1)
-
-puts "\n\nSetting parameter #{param} to 1"
-puts "=================================="
-VMR._set_parameter(param, 1)
-VMR._write_cache(param)
-newvalue = VMR._load_cache(param)
-puts "#{param} = #{newvalue}"
-
-sleep(1)
-
-puts "Setting parameter #{param} to 0"
-puts "================================="
-VMR._set_parameter(param, 0)
-VMR._write_cache(param)
-newvalue = VMR._load_cache(param)
-puts "#{param} = #{newvalue}"
-
-sleep(1)
-
-puts "\nParameters set. Current cache:\n"
-puts "================================="
-full_cache = VMR._load_cache(nil, 1)
-puts "#{full_cache}"
-
-sleep(1)
-
-puts "\n\nShowing VBAN Chat"
-puts "================================="
-VMR._special_command('DialogShow.VBANCHAT')
-
-sleep(1)
-
-# only potato supports the recorder
-if VMR._vbType == 3
-    VMR._recorder('play')
-    sleep(20)
-    VMR._recorder('stop')
-
-    sleep(1)
-end
-
-puts "\n\nSave settings to config"
-puts "================================="
-VMR._save_config
-
-VMR._logout
-
-
-=begin
-    note _get_parameter working reliably if value set 
-    in script but not if change in GUI
-    special commands are readonly
-
-    name.gsub(/[\[|\]]/, "|")
-=end
