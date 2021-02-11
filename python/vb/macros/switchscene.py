577e7b749bf0f2fc458833cb99e2ffe8a4be15e8
Onyx and Iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-02-11 17:34:51 +0000

    Update switchscene.py
    move switch scene functions into class
    
    serialize password write to pickle, run set pass if pass_file does not exist.

--- python/vb/macros/switchscene.py
+++ python/vb/macros/switchscene.py
@@ -1,22 +1,52 @@
 import sys
 import time
+import pickle
 
 sys.path.append('../')
 from obswebsocket import obsws, requests
 
-def switch_to(name):
-  host = "localhost"
-  port = 4444
-  password = "52RKcvcglccfcurveiucebrunjjdbitv"
+pass_file = "pwd.pkl"
+class obs_sceneswitch:
+  def __init__(self):
+    self.pass_file = "pwd.pkl"
 
-  ws = obsws(host, port, password)
-  ws.connect()
+  def switch_to(self, name):
+    host = "localhost"
+    port = 4444
+    
+    try:
+        with open(self.pass_file, 'rb') as retrieve_pass:
+          password = pickle.load(retrieve_pass)
+          retrieve_pass.close()
 
-  try:
-      scenes = ws.call(requests.GetSceneList())
-      ws.call(requests.SetCurrentScene(name))
+    except FileNotFoundError:
+      create_pass()
 
-  except KeyboardInterrupt:
-      pass
+    ws = obsws(host, port, password)
+    ws.connect()
 
-  ws.disconnect()
+    try:
+        scenes = ws.call(requests.GetSceneList())
+        ws.call(requests.SetCurrentScene(name))
+
+    except KeyboardInterrupt:
+        pass
+
+    ws.disconnect()
+    
+  def create_pass(self):
+    password = input("Enter OBS websocket password\n")
+    while True:
+      try:
+        with open(self.pass_file, 'wb') as save_pass:
+          pickle.dump(password, save_pass)  
+          save_pass.close()
+          break
+          
+      except FileNotFoundError:
+        save_pass = open(self.pass_file, 'x')
+        save_pass.close()
+  
+if __name__ == '__main__':
+  make_pass = obs_sceneswitch()
+  make_pass.create_pass()
