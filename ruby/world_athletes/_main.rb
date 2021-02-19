3fe0f229b802e0ca21e0ff3ca7c7e431e6dc422e
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-02-19 16:31:03 +0000

    update world runners
    Added Swimmers data
    
    Added Swimmer subclass.
    Factory functions now used in Swimmer and Cyclist
    Attributes initialized using inheritance and method override
    
    Might shift team writer method to superclass later on.

--- ruby/world_athletes/_main.rb
+++ ruby/world_athletes/_main.rb
@@ -1,148 +1,180 @@
 require_relative "athletes"
 
 format_separator = "======================"
+############### RUNNERS ##################
+Laura = Runner.new("Laura", "Muir", 28)
+Laura.country = "Scotland"
+Laura.event = "1500m"
+Laura.pb = "3:55"
 
-laura = Runner.new
-laura.name_first = "Laura"
-laura.name_last = "Muir"
-laura.age = 28
-laura.country = "Scotland"
-laura.event = "1500m"
-laura.pb = 3.55
+Mo = Runner.new("Mohammad", "Farah", 38)
+Mo.country = "England"
+Mo.event = "10000m"
+Mo.pb = "26:46"
 
-mo = Runner.new
-mo.name_first = "Mohammad"
-mo.name_last = "Farah"
-mo.age = 38
-mo.country = "England"
-mo.event = "10000m"
-mo.pb = 24.00
+Dina = Runner.new("Dina", "Asher-Smith", 25)
+Dina.country = "England"
+Dina.event = "100m"
+Dina.pb = "10.83"
 
-dina = Runner.new
-dina.name_first = "Dina"
-dina.name_last = "Asher Smith"
-dina.age = 28
-dina.country = "England"
-dina.event = "100m"
-dina.pb = 10.83
+Adam = Runner.new("Adam", "Gemili", 27)
+Adam.country = "England"
+Adam.event = "100m"
+Adam.pb = "9.97"
 
-laura.versus(mo)
-puts format_separator
-dina.versus(laura)
-puts format_separator
-mo.versus(dina)
-puts format_separator
-laura.isWanda
-puts format_separator
+Amy = Runner.new("Amy", "Hunt", 18)
+Amy.country = "England"
+Amy.event = "200m"
+Amy.pb = "22.42"
 
+############### CYCLISTS ##################
 # AG2R La Mondiale
-romain = Cyclist.new
-romain.name_first = "Romain"
-romain.name_last = "Bardet"
-romain.team = "AG2R La Mondiale"
+romain = Cyclist.ag2r("Romain", "Bardet")
 romain.rank = 1
 romain.career_points = 9181
 
-Tony = Cyclist.new
-Tony.name_first = "Tony"
-Tony.name_last = "Galllopin"
-Tony.team = "AG2R La Mondiale"
+Tony = Cyclist.ag2r("Tony", "Gallopin")
 Tony.rank = 2
 Tony.career_points = 7060
 
-Oliver = Cyclist.new
-Oliver.name_first = "Oliver"
-Oliver.name_last = "Naesen"
-Oliver.team = "AG2R La Mondiale"
+Oliver = Cyclist.ag2r("Oliver", "Naesen")
 Oliver.rank = 3
 Oliver.career_points = 4937
 
-
-Mathias = Cyclist.new
-Mathias.name_first = "Oliver"
-Mathias.name_last = "Naesen"
-Mathias.team = "AG2R La Mondiale"
+Mathias = Cyclist.ag2r("Frank", "Mathias")
 Mathias.rank = 4
 Mathias.career_points = 4239
 
-Frank = Cyclist.new
-Frank.name_first = "Frank"
-Frank.name_last = "Mathias"
-Frank.team = "AG2R La Mondiale"
+Frank = Cyclist.ag2r("Alexis", "Vuillermoz")
 Frank.rank = 5
-Frank.career_points = 4239
+Frank.career_points = 3003
 
 # NTT Pro Cycling
-Edvald = Cyclist.new
-Edvald.name_first = "Edvald"
-Edvald.name_last = "Hagen"
-Edvald.team = "NTT Pro Cycling"
+Edvald = Cyclist.ntt("Edvald", "Hagen")
 Edvald.rank = 1
 Edvald.career_points = 13844
 
-Roman = Cyclist.new
-Roman.name_first = "Roman"
-Roman.name_last = "Kreuziger"
-Roman.team = "NTT Pro Cycling"
+Roman = Cyclist.ntt("Roman", "Kreuziger")
 Roman.rank = 2
 Roman.career_points = 10218
 
-Domenico = Cyclist.new
-Domenico.name_first = "Domenico"
-Domenico.name_last = "Pozzovivo"
-Domenico.team = "NTT Pro Cycling"
+Domenico = Cyclist.ntt("Domenico", "Pozzovivo")
 Domenico.rank = 3
 Domenico.career_points = 9824
 
-Giacomo = Cyclist.new
-Giacomo.name_first = "Giacomo"
-Giacomo.name_last = "Nizzolo"
-Giacomo.team = "NTT Pro Cycling"
+Giacomo = Cyclist.ntt("Giacomo", "Nizzolo")
 Giacomo.rank = 4
 Giacomo.career_points = 6458
 
-Enrico = Cyclist.new
-Enrico.name_first = "Enrico"
-Enrico.name_last = "Gasparott"
-Enrico.team = "NTT Pro Cycling"
+Enrico = Cyclist.ntt("Enrico", "Gasparott")
 Enrico.rank = 5
 Enrico.career_points = 5787
 
 # Trek-Segafredo
-Vincenzo = Cyclist.new
-Vincenzo.name_first = "Vincenzo"
-Vincenzo.name_last = "Nibali"
-Vincenzo.team = "Trek-Segafredo"
+Vincenzo = Cyclist.trek("Vincenzo", "Nibali")
 Vincenzo.rank = 1
 Vincenzo.career_points = 19153
 
-Bauke = Cyclist.new
-Bauke.name_first = "Bauke"
-Bauke.name_last = "Mollema"
-Bauke.team = "Trek-Segafredo"
+Bauke = Cyclist.trek("Bauke", "Mollema")
 Bauke.rank = 2
 Bauke.career_points = 11627
 
-Jasper = Cyclist.new
-Jasper.name_first = "Jasper"
-Jasper.name_last = "Stuyven"
-Jasper.team = "Trek-Segafredo"
+Jasper = Cyclist.trek("Jasper", "Stuyven")
 Jasper.rank = 3
 Jasper.career_points = 5736
 
-Mads = Cyclist.new
-Mads.name_first = "Mads"
-Mads.name_last = "Pedersen"
-Mads.team = "Trek-Segafredo"
+Mads = Cyclist.trek("Mads", "Pedersen")
 Mads.rank = 4
 Mads.career_points = 4307
 
-Gianluca = Cyclist.new
-Gianluca.name_first = "Gianluca"
-Gianluca.name_last = "Brambilla"
-Gianluca.team = "Trek-Segafredo"
+Gianluca = Cyclist.trek("Gianluca", "Brambilla")
 Gianluca.rank = 5
 Gianluca.career_points = 3327
+
+############### SWIMMERS ##################
+# London Roar
+Adam = Swimmer.lonr("Adam", "Peaty")
+Adam.aggregate = 36854
+Adam.pb = "4294"
+Adam.event = "100m Breastroke"
+
+Guilherme = Swimmer.lonr("Guilherme", "Guido")
+Guilherme.aggregate = 33585
+Guilherme.pb = "9170"
+Guilherme.event = "50m Backstroke"
+
+Alia = Swimmer.lonr("Alia", "Atkinson")
+Alia.aggregate = 32265
+Alia.pb = "4385"
+Alia.event = "50m Breastroke"
+
+Sydney = Swimmer.lonr("Sydney", "Pickrem")
+Sydney.aggregate = 30364
+Sydney.pb = "4053"
+Sydney.event = "200m Medley"
+
+Minna = Swimmer.lonr("Minna", "Atherton")
+Minna.aggregate = 28910
+Minna.pb = "10830"
+Minna.event = "100m Backstroke"
+
+# NY Breakers
+Michael = Swimmer.nyb("Michael", "Andrew")
+Michael.aggregate = 31288
+Michael.pb = "2706"
+Michael.event = "50m Butterfly"
+
+Marco = Swimmer.nyb("Marco", "Koch")
+Marco.aggregate = 25555
+Marco.pb = "3033"
+Marco.event = "200m Breaststroke"
+
+Arina = Swimmer.nyb("Arina", "Surkova")
+Arina.aggregate = 23869
+Arina.event = "50m Butterfly"
+
+Kasia = Swimmer.nyb("Kasia", "Wasick")
+Kasia.aggregate = 32724
+
+Joe = Swimmer.nyb("Joe", "Litchfield")
+Joe.aggregate = 23089
+Joe.event = "Freestyle"
+
+# DC Trident
+Beth = Swimmer.oct("Bethany", "Galat")
+Beth.aggregate = 22698
+Beth.pb = "2499"
+Beth.event = "100m Breaststroke"
+
+Zach = Swimmer.oct("Zach", "Apple")
+Zach.aggregate = 22436
+Zach.pb = "1583"
+Zach.event = "100m Freestyle"
+
+AmyB = Swimmer.oct("Amy", "Bilquist")
+AmyB.aggregate = 21512
+AmyB.pb = "2710"
+AmyB.event = "100m Backstroke"
+
+Zane = Swimmer.oct("Zane", "Grothe")
+Zane.aggregate = 20718
+Zane.pb = "3823"
+Zane.event = "400m Freestyle"
+
+Lindsey = Swimmer.oct("Lindsey", "Kozelsky")
+Lindsey.aggregate = 20580
+
+######################################
+
+Laura.versus(Mo)
+puts format_separator
+Dina.versus(Laura)
+puts format_separator
+Mo.versus(Dina)
+puts format_separator
+Laura.is_wanda
+puts format_separator
+puts "Lauras pb is: #{Laura.pb}"
 
 puts "\n\n" + format_separator
 puts format_separator
@@ -151,3 +183,11 @@ puts format_separator
 Edvald.team
 puts format_separator
 Jasper.team
+
+puts "\n\n" + format_separator
+puts format_separator
+
+Alia.versus(Arina)
+Zane.versus(Michael)
+Kasia.versus(Minna)
+
--- ruby/world_athletes/athletes.rb
+++ ruby/world_athletes/athletes.rb
@@ -1,8 +1,8 @@
 class Athlete
 	""" Superclass  Athlete """
-	attr_accessor :name_first, :name_last, :country, :event
-	attr_reader :age, :pb
-
+	attr_accessor :country, :event
+	attr_reader :name_first, :name_last, :age, :pb
+ 
 	def age=(value)
 		""" writer method validate input """
 		if value < 0
@@ -12,10 +12,34 @@ class Athlete
 	end
 	
 	def pb=(value)
-		if value < 0
-			raise "ERROR: age must be a positive value"
+		""" writer method validate input """
+		if value.empty?
+			raise "ERROR: pb cannot be blank!"
 		end
 		@pb = value
+	end
+
+	def name_first=(value)
+		""" writer method validate input """
+		if value.empty?
+			raise "ERROR: Name cannot be blank string!"
+		end
+		@name_first = value
+	end
+
+	def name_last=(value)
+		""" writer method validate input """
+		if value.empty?
+			raise "ERROR: Name cannot be blank string!"
+		end
+		@name_last = value
+	end
+
+	def initialize(name_first = "Default", name_last = "Name")
+		""" alias to writer methods """
+		self.name_first = name_first
+		self.name_last = name_last
+		self.event = "Unknown"
 	end
 
 	def fullname
@@ -28,21 +52,52 @@ class Athlete
 	end
 end
 
+
 class Runner < Athlete
 	""" subclass runner """
-	def isWanda
+	def initialize(name_first = "Default", name_last = "Name", age = 0)
+		""" call super with only name_first and name_last """
+		super(name_first, name_last)
+		self.age = age
+	end
+
+	def is_wanda
 		puts "#{fullname} is a member of the Wanda Diamond League"
 	end
 end
 
-class Swimmer < Athlete
-	""" subclass swimmer """
-end
 
 class Cyclist < Athlete
 	""" subclass cyclist """
-	attr_accessor :team
-	attr_reader :rank, :career_points
+	attr_reader :rank, :career_points, :team
+
+	def team=(value)
+		if value.empty?
+			raise "ERROR Team cannot be blank string!"
+		end
+		@team = value
+	end
+
+	def self.ag2r(name_first, name_last)
+		""" factory function """
+		Cyclist.new(name_first, name_last, "AG2R La Mndoiale")
+	end
+
+	def self.ntt(name_first, name_last)
+		""" factory function """
+		Cyclist.new(name_first, name_last, "NTT Pro Cycling")
+	end
+
+	def self.trek(name_first, name_last)
+		""" factory function """
+		Cyclist.new(name_first, name_last, "Trek-Segafredo")
+	end
+
+	def initialize(name_first, name_last, team = "Default")
+		""" call super with only name_first and name_last """
+		super(name_first, name_last)
+		self.team = team
+	end
 
 	def rank=(value)
 		if value < 0
@@ -72,5 +127,51 @@ class Cyclist < Athlete
 	def team
 		puts "#{fullname} is a member of #{@team}"
 		puts "Their team rides bikes: #{bike(@team)}"
+	end
+end
+
+class Swimmer < Athlete
+	attr_accessor :aggregate, :team, :stroke, :distance
+
+	""" subclass swimmer """
+	def team=(value)
+		if value.empty?
+			raise "ERROR Team cannot be blank string!"
+		end
+		@team = value
+	end
+
+	def self.lonr(name_first, name_last)
+		""" factory function """
+		Swimmer.new(name_first, name_last, "London Roar")
+	end
+
+	def self.nyb(name_first, name_last)
+		""" factory function """
+		Swimmer.new(name_first, name_last, "New York Breakers")
+	end
+
+	def self.oct(name_first, name_last)
+		""" factory function """
+		Swimmer.new(name_first, name_last, "OC Trident")
+	end
+
+	def initialize(name_first, name_last, team = "Default")
+		""" call super with only name_first and name_last """
+		super(name_first, name_last)
+		self.team = team
+	end
+
+	def versus(competitor)
+		""" method override """
+		@distance, @stroke = @event.split()
+		competitor.distance, competitor.stroke = competitor.event.split()
+
+		if self.stroke.nil? || competitor.stroke.nil?
+			puts("Comparing #{fullname} vs #{competitor.fullname}")
+			puts("Event not known for one of the athletes")
+		else
+			super
+		end
 	end
 end
