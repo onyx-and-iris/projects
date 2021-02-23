03f8e48a3cbf05f735601000af9b645b48bea79f
onyx <onyx@onxyandiris.online>
2021-02-23 18:05:31 +0000

    update to runners.py
    Added get_records_byname. Stored records can now be retrieved using shortname query. Added -get argument variable which can take all or shortname as an option argument. Cleaned up some codestyle on original program

--- python/world_runners_III/runners.py
+++ python/world_runners_III/runners.py
@@ -1,15 +1,13 @@
 import pickle
 import argparse
-from pathlib import Path
+
+from sys import exit
 
 
 class FileOps:
     def __init__(self):
         self.db = {}
-        self.file_db = 'records.db'
-
-    def create_empty(self):
-        Path(self.file_db).touch()
+        self.file_db = 'records.pkl'
 
     def write_file(self):
         records = open(self.file_db, 'wb')
@@ -23,8 +21,9 @@ class FileOps:
             with open(self.file_db, 'rb') as records:
                 self.db = pickle.load(records)
 
-        except IOError:
-            self.createEmpty()
+        except FileNotFoundError:
+            with open(self.file_db, 'x') as records:
+                pass
 
         except EOFError:
             pass
@@ -32,15 +31,15 @@ class FileOps:
         return self.db
 
     def truncate(self):
-        records = open(self.file_db, 'wb')
-        records.truncate(0)
-        records.close
+        with open(self.file_db, 'wb') as records:
+            records.truncate(0)
 
 
 class Records:
     def __init__(self):
-        file_io = FileOps()
-        self.db = file_io.read_file()    
+        self.file_io = FileOps()
+        self.db = self.file_io.read_file()    
+        self.options = {key for key in self.db}
 
     def new_record(self):
         next = 'y'
@@ -61,19 +60,31 @@ class Records:
 
             next = input('Add another record (y/n)? ')
 
-        file_io.write_file()
+        self.file_io.write_file()
 
     def get_records(self):
         if not self.db:
             print('No records set yet, please set some!')
         else:
-            for key in self.db:
-                print(f'{self.db[key]["name"]} \n==============\n'
-                f'{self.db[key]["age"]}\n{self.db[key]["country"]}\n'
-                f'{self.db[key]["event"]}\n{self.db[key]["pb"]}\n')
+            for runner in self.options:
+                print(f'=======================')
+                for key, val in self.db[runner].items():
+                    print(key, val)
+
+    def get_record_byname(self, name):
+        if not self.db:
+            print('No records set yet, please set some!')
+        else:
+            try:
+                return {k:v for (k,v) in self.db[name].items()}
+            except (KeyError, ValueError) as e:
+                print(f'Error: no record for {name}\n'
+                f'Available options: {self.options}')
+                exit()
+        
 
     def reset():
-        file_io = FileOps()
+        self.file_io = FileOps()
         response = input('Truncating database, are you sure? (y/n) ')
         if response == 'y':
             file_io.truncate()
@@ -82,15 +93,23 @@ class Records:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', action='store_true')
-    parser.add_argument('-g', action='store_true')
-    parser.add_argument('-t', action='store_true')
+    parser.add_argument('-get', nargs=None)
+    parser.add_argument('-r', action='store_true')
     args = parser.parse_args()
 
     records = Records()
+    record = []
 
     if args.n:
         records.new_record()
-    elif args.g:
-        records.get_records()
-    elif args.t:
+    elif args.get:
+        arg = args.get
+
+        if arg == 'all':
+            records.get_records()
+        else:
+            record = records.get_record_byname(arg)
+            for key in record:
+                print(f'{key:8}  :  {record[key]}')
+    elif args.r:
         records.reset()
