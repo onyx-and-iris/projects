5f82df402a8c43b41f2ba729266c9c932cc597d6
Onyx and Iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-03-05 10:34:32 +0000

    Update main.py
    Added reset function for reset arg.

--- python/vb/macros/main.py
+++ python/vb/macros/main.py
@@ -1,119 +1,93 @@
 import macrobuttons
 import voicemeeter
 import argparse
-import pickle
 
+from sys import exit
+from custom import FileOps
 
-class FileOps:
-  """ save/retrieve states to pickle file """
-  def __init__(self, macros):
-    self.db = macros
-    self.file_db = 'cache.pkl'
-
-  def read_db(self):
-    while True:
-      try:
-        with open(self.file_db, 'rb') as records:
-          self.db = pickle.load(records)
-        break
-
-      except FileNotFoundError:
-        records = open(self.file_db, 'x')
-        records.close()
-
-      except EOFError:
-        self.update_db(self.db)
-        break
-
-    return self.db
-
-  def update_db(self, macros):
-    with open(self.file_db, 'wb') as records:
-      pickle.dump(macros, records)
-
+def reset():
+    with voicemeeter.remote('potato') as oai:
+        reset = macrobuttons.Reset('reset', 0, oai)
+        reset.reset(macros)
+    file_io.update_db(macros)
+    exit()
 
 def main(layer, arg):
-  """ set macro + switch vars, load save states,
-  call desired class/method then update save states """
-  this_macro = macros[layer][arg][0]
-  switch = macros[layer][arg][1]
+    """ set macro + switch vars, load save states,
+    call desired class/method then update save states """
+    this_macro = macros[layer][arg][0]
+    switch = macros[layer][arg][1]
 
-  """ get saved states from pickle file """
-  saved_states = file_io.read_db()
-  saved_state = saved_states[layer][arg][1]
+    """ get saved states from pickle file """
+    saved_states = file_io.read_db()
+    saved_state = saved_states[layer][arg][1]
 
-  if switch == saved_state:
-    switch = 1 - switch
+    if switch == saved_state:
+        switch = 1 - switch
 
-  """ Do we need Audio or Scenes class? Then instantiate """  
-  by_class = getattr(macrobuttons, layer.capitalize())
+    """ Do we need Audio or Scenes class? Then instantiate """    
+    by_class = getattr(macrobuttons, layer.capitalize())
 
-  with voicemeeter.remote('potato') as oai:
-    """ call .run() for appropriate class """  
-    by_class(this_macro, switch, oai).run()
+    with voicemeeter.remote('potato') as oai:
+        """ call .run() for appropriate class """    
+        by_class(this_macro, switch, oai).run()
 
-  saved_states[layer][arg][1] = switch
+    saved_states[layer][arg][1] = switch
 
-  """ write updates to file """
-  file_io.update_db(saved_states)
+    """ write updates to file """
+    file_io.update_db(saved_states)
 
 
 if __name__ == '__main__':
-  """ Initiate the parser """
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-audio', nargs=None,
-  choices=['mm', 'od', 'os', 'st', 'so', 'si'])
-  parser.add_argument('-scenes', nargs=None,
-  choices=['oo', 'io', 'ds', 'ob', 'ib', 'start', 'brb', 'end'])
-  parser.add_argument('-reset', action='store_true')
+    """ Initiate the parser """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-audio', nargs=None,
+    choices=['mm', 'od', 'os', 'st', 'so', 'si'])
+    parser.add_argument('-scenes', nargs=None,
+    choices=['oo', 'io', 'ds', 'ob', 'ib', 'start', 'brb', 'end'])
+    parser.add_argument('-reset', action='store_true')
 
-  """ Read arguments from the command line """
-  args = parser.parse_args()
+    """ Read arguments from the command line """
+    args = parser.parse_args()
 
-  audio = {
-      'mm': ['mute_mics', 1], 
-      'od': ['only_discord', 0], 
-      'os': ['only_stream', 1], 
-      'st': ['sound_test', 0], 
-      'so': ['solo_onyx', 0], 
-      'si': ['solo_iris', 0]
-  }
+    audio = {
+        'mm': ['mute_mics', 1], 
+        'od': ['only_discord', 0], 
+        'os': ['only_stream', 1], 
+        'st': ['sound_test', 0], 
+        'so': ['solo_onyx', 0], 
+        'si': ['solo_iris', 0]
+    }
 
-  scenes = { 
-      'oo': ['onyx_only', 0], 
-      'io': ['iris_only', 0], 
-      'ds': ['dual_scene', 0], 
-      'ob': ['onyx_big', 0], 
-      'ib': ['iris_big', 0],
-      'start': ['start', 0],
-      'brb': ['brb', 0],
-      'end': ['end', 0]
-  }
+    scenes = { 
+        'oo': ['onyx_only', 0], 
+        'io': ['iris_only', 0], 
+        'ds': ['dual_scene', 0], 
+        'ob': ['onyx_big', 0], 
+        'ib': ['iris_big', 0],
+        'start': ['start', 0],
+        'brb': ['brb', 0],
+        'end': ['end', 0]
+    }
 
-  gamecaster = {}
+    gamecaster = {}
 
-  macros = {}
-  macros['audio'] = audio
-  macros['scenes'] = scenes
-  macros['gamecaster'] = gamecaster
+    macros = {}
+    macros['audio'] = audio
+    macros['scenes'] = scenes
+    macros['gamecaster'] = gamecaster
 
-  file_io = FileOps(macros) 
+    file_io = FileOps(macros) 
 
-  if args.audio:
-    layer = 'audio'
-    arg = args.audio
+    if args.reset:
+        reset()
 
+    elif args.audio:
+        layer = 'audio'
+        arg = args.audio
+
+    elif args.scenes:
+        layer = 'scenes'
+        arg = args.scenes
+        
     main(layer, arg)
-
-  elif args.scenes:
-    layer = 'scenes'
-    arg = args.scenes
-
-    main(layer, arg)
-
-  elif args.reset:
-    with voicemeeter.remote('potato') as oai:
-      macro = macrobuttons.Scenes('reset', 0, oai)
-      macro.reset(macros)
-    
-    file_io.update_db(macros)
