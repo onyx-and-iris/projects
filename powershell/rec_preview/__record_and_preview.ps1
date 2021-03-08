ddec94a406c2b96ab9e5ea4034469f78a7f7346e
Onyx and Iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-03-08 21:23:44 +0000

    Update __record_and_preview.ps1
    Script now reads ffmpeg/ffplay paths from config file
    
    Now saving dshow devices to string array.
    
    Fixed building of audio string.
    
    Added exception handling to Stop-Job
    
    Stop function now moves recorded files to backup files.
    
    TODO: have backup files transferred over network for DNxHD conversion.

--- powershell/rec_preview/__record_and_preview.ps1
+++ powershell/rec_preview/__record_and_preview.ps1
@@ -1,70 +1,149 @@
 param([switch]$rec, [switch]$stop)
 
 Function Show{
-	Start-Job -Name play_top -ScriptBlock {
-		D:\play_top.ps1 -capture $args[0] -ffplay $args[1]
+    param($FF, [string[]]$CAPTURE, [string[]]$SCRIPTS)
 
-	} -ArgumentList $CAPTURE[0], $FFPLAY
+    # Setup script arguments for play_top
+    $run = "play_top"
+    FOREACH ($file in $SCRIPTS) {
+        if ($file -Match $run) {
+            $THIS_SCRIPT = $file
+        }
+    }
+    Start-Job -Name $run -ScriptBlock {
+        & $args[2] -capture $args[0] -ffplay $args[1]
+    } -ArgumentList $CAPTURE[0], $FF.FFPLAY, $THIS_SCRIPT
 
-	Start-Job -Name play_front -ScriptBlock {
-		D:\play_front.ps1 -capture $args[0] -ffplay $args[1]
-
-	} -ArgumentList $CAPTURE[1], $FFPLAY
-
-	if($rec) { 
-		Rec
-	}
+    # Setup script arguments for play_front
+    $run = "play_front"
+    FOREACH ($file in $SCRIPTS) {
+        if ($file -Match $run) {
+            $THIS_SCRIPT = $file
+        }
+    }
+	Start-Job -Name $run -ScriptBlock {
+        & $args[2] -capture $args[0] -ffplay $args[1]
+	} -ArgumentList $CAPTURE[1], $FF.FFPLAY, $THIS_SCRIPT
 }
 
 Function Rec{
-	Start-Job -Name rec_top -ScriptBlock {
-		D:\rec_top.ps1 -capture $args[0] -ffmpeg $args[1]
+    param($FF, [string[]]$CAPTURE, [string[]]$SCRIPTS, $AUDIO)
+    New-Item -ItemType Directory -Force -Path ".\rec"
 
-	} -ArgumentList $CAPTURE[0], $FFMPEG
+    # Setup script arguments for play_top
+    $run = "rec_top"
+    FOREACH ($file in $SCRIPTS) {
+        if ($file -Match $run) {
+            $THIS_SCRIPT = $file
+        }
+    }
+	Start-Job -Name $run -ScriptBlock {
+		& $args[2] -capture $args[0] -ffmpeg $args[1]
 
-	Start-Job -Name rec_front -ScriptBlock {
-		D:\rec_front.ps1 -capture $args[0] -ffmpeg $args[1]
+	} -ArgumentList $CAPTURE[0], $FF.FFMPEG, $THIS_SCRIPT
 
-	} -ArgumentList $CAPTURE[1], $FFMPEG
+    # Setup script arguments for play_front
+    $run = "rec_front"
+    FOREACH ($file in $SCRIPTS) {
+        if ($file -Match $run) {
+            $THIS_SCRIPT = $file
+        }
+    }
+	Start-Job -Name $run -ScriptBlock {
+		& $args[2] -capture $args[0] -ffmpeg $args[1]
 
-	Start-Job -Name rec__mics -ScriptBlock {
-		D:\rec__mics.ps1 -audio $args[0] -ffmpeg $args[1]
+	} -ArgumentList $CAPTURE[1], $FF.FFMPEG, $THIS_SCRIPT
 
-	} -ArgumentList $AUDIO, $FFMPEG
+    # Record mics
+    $run = "rec__mics"
+    FOREACH ($file in $SCRIPTS) {
+        if ($file -Match $run) {
+            $THIS_SCRIPT = $file
+        }
+    }
+	Start-Job -Name $run -ScriptBlock {
+		& $args[2] -mics $args[0] -ffmpeg $args[1]
+
+	} -ArgumentList $AUDIO, $FF.FFMPEG, $THIS_SCRIPT
 }
 
 Function Stop{
-	Write-Host "Stopping jobs"
-	Stop-Job play_top, play_front, rec_top, rec_front, rec__mics
-	exit
+    [string[]]$jobs = @("play_top", "play_front", "rec_top", "rec_front", "rec__mics")
+    FOREACH ($job in Get-Job -State 'Running')
+    {
+        if ($jobs.Contains($job.name)) {
+            Write-Host "Stopping ", $job.name
+            Stop-Job $job
+        }
+    }
+
+    Get-ChildItem ./rec/ -recurse | where {$_.extension -in ".mkv",".wav"} | % {
+        $i = 1
+        $StopLoop = $false
+        do {
+            try {
+                Rename-Item -Path $_.FullName `
+                -NewName "$($_.FullName).backup.$i" -ErrorAction 'Stop'
+                $StopLoop = $true
+            } 
+            catch {
+                Start-Sleep -m 100
+                $i++
+            }
+        } until ($StopLoop -eq $true)
+        
+        [string[]]$OUTFILES += $_.FullName
+    }
+    FOREACH ($string in $OUTFILES) {
+        $savefile = Split-Path $string -leaf
+        Write-Host "Backed up file: ", $savefile
+    }
+
+	Exit
 }
 
+
 if ($MyInvocation.InvocationName -ne '.')
-{
-	if($stop) { 
-		Stop
+{  
+    if($stop) { 
+      Stop
+    }
+
+    $CONFIG_FILE = 'config.txt'
+    $CAPTURE_CARD_PATTERN = '*Game Capture*Video*'
+    $AUDIO_DEVICE_PATTERN = '*Mic/Line In 05/06*'
+
+
+    # Load ffmpeg settings from config file, save to Hash
+    $FF = Get-Content $CONFIG_FILE | ConvertFrom-StringData
+
+    # Store dshow devices to Array
+    $DATA = & $FF.FFMPEG -list_devices true -f dshow -i dummy 2>&1
+    FOREACH ($line in $DATA) {
+        if ($line -like $CAPTURE_CARD_PATTERN) {
+            $line = $($line | Out-String).split('"')[1]
+            [string[]]$CAPTURE += $line
+        }
+
+        if ($line -like $AUDIO_DEVICE_PATTERN) {
+            $line = $($line | Out-String).split('"')[1].Trim()
+            $AUDIO = $line.replace("`n",'').replace("`r",'')
+        }
+    }
+
+    Write-Host "Using Devices:"
+    FOREACH ($string in $CAPTURE) {
+        Write-Host $string
+    }
+    Write-Host $AUDIO
+    
+    # Get all script files in working directory, save to string array
+    Get-ChildItem ./ -recurse | where {$_.extension -eq ".ps1"} | % {
+        [string[]]$SCRIPTS += $_.FullName
+    }
+
+    Show -FF $FF -CAPTURE $CAPTURE -SCRIPTS $SCRIPTS
+    if($rec) { 
+		Rec -FF $FF -CAPTURE $CAPTURE -SCRIPTS $SCRIPTS -AUDIO $AUDIO
 	}
-	
-	$FFPLAY="${PSScriptRoot}\ffmpeg\ffmpeg\bin\ffplay.exe"
-	$FFMPEG="${PSScriptRoot}\ffmpeg\ffmpeg\bin\ffmpeg.exe"
-
-	"Write ffmpeg direct show devices to file"
-	& $FFMPEG -list_devices true -f dshow -i dummy > ${PSScriptRoot}\ffmpeg.txt 2>&1
-
-	"Get the capture card devices and save to array"
-	[string[]]$CAPTURE=$(select-string -Path ffmpeg.txt -Pattern '(Game Capture.*Video)')
-	"Parse array"
-	$CAPTURE = $CAPTURE | ForEach-Object { $_.split('"')[1] }
-
-	"Save audio device from file into string"
-	$AUDIO = $(select-string -Path ffmpeg.txt -Pattern 'Mic/Line In 05/06') | Out-String 
-	$AUDIO = $AUDIO.split('"')[1]
-
-	Write-Host "Using Devices:"
-	FOREACH ($string in $CAPTURE) {
-		Write-Host $string
-	}
-	Write-Host $AUDIO
-	
-	Show
 }
