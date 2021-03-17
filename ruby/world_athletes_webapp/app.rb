20c0a29241e88423ea982eb8c28c53d7bf948f33
onyx <onyx@onxyandiris.online>
2021-03-17 17:38:25 +0000

    update to app.rb
    Added get method to create another runner object. Added post method to save entered data into new object. Added get method to retrieve data from yaml to be displayed on a page by id

--- ruby/world_athletes_webapp/app.rb
+++ ruby/world_athletes_webapp/app.rb
@@ -1,23 +1,34 @@
 require 'sinatra'
 require 'athletes'
+require 'yaml_store'
+
+store = AthleteStore.new('athletes.yml')
 
 get('/runners') do
-    @Laura = Runner.new("Laura", "Muir", 28,
-    country: "Scotland", event: "1500m", pb: "3:55")
-    @Mo = Runner.new("Mohammad", "Farah", 38, 
-    country: "England", event: "10000m", pb: "26:46")
-    @Dina = Runner.new("Dina", "Asher-Smith", 25,
-    country: "England", event: "100m", pb: "10.83")
-    @Adam = Runner.new("Adam", "Gemili", 27, 
-    country: "England", event: "100m", pb: "9.97")
-    @Amy = Runner.new("Amy", "Hunt", 18,
-    country: "England", event: "200m", pb: "22.42")
-    @runners = [@Laura, @Mo, @Dina, @Adam, @Amy]
-
+    @runners = store.show
     erb :index
 end
 
 get('/runners/new') do
     erb :new
+end
+
+post('/runners/create') do
+    @runner = Runner.new(
+    params['name_first'],
+    params['name_last'],
+    params['age'].to_i,
+    country: params['country'],
+    event: params['event'],
+    pb: params['pb']
+    )
+    store.save(@runner)
+    redirect 'runners/new'
+end
+
+get('/runners/:id') do
+    id = params['id'].to_i
+    @runner = store.find(id)
+    erb :show
 end
 
