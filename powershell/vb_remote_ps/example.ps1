dac967d990cf9e3eeca0ebf919853f9ad4629f65
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-04-28 16:30:14 +0100

    Update example.ps1
    sourcing voicemeeter.ps1 now logs in.
    
    added try, finally blocks to example.

--- powershell/vb_remote_ps/example.ps1
+++ powershell/vb_remote_ps/example.ps1
@@ -1,15 +1,22 @@
-. $PSScriptRoot\lib\voicemeeter.ps1
+try {
+    . $PSScriptRoot\lib\voicemeeter.ps1
 
-Login
+    $button[0].state = 1
+    $button[0].state
+    $button[0].state = 0
+    $button[0].state
 
-$button[0].state = 1
-$button[0].state
-$button[0].state = 0
-$button[0].state
+    $strip[0].mono = 1
+    $strip[0].mono
+    $strip[0].mono = 0
+    $strip[0].mono
 
-$strip[0].mono = 1
-$strip[0].mono
-$strip[0].mono = 0
-$strip[0].mono
-
-Logout
+    $bus[2].mute = 1
+    $bus[2].mute
+    $bus[2].mute = 0
+    $bus[2].mute
+}
+finally
+{
+    Logout
+}
