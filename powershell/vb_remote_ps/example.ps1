674cacd4c189772f4ebf3f454f9dcb896b7d26fc
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-04-28 17:05:17 +0100

    Update example.ps1
    added try, finally to example.
    
    login can now be called with a vb type

--- powershell/vb_remote_ps/example.ps1
+++ powershell/vb_remote_ps/example.ps1
@@ -1,22 +1,26 @@
 try {
     . $PSScriptRoot\lib\voicemeeter.ps1
 
-    $button[0].state = 1
-    $button[0].state
-    $button[0].state = 0
-    $button[0].state
+    $vmr = [Remote]::new('potato')
 
-    $strip[0].mono = 1
-    $strip[0].mono
-    $strip[0].mono = 0
-    $strip[0].mono
+    $vmr.Login()
 
-    $bus[2].mute = 1
-    $bus[2].mute
-    $bus[2].mute = 0
-    $bus[2].mute
+    $vmr.button[0].state = 1
+    $vmr.button[0].state
+    $vmr.button[0].state = 0
+    $vmr.button[0].state
+
+    $vmr.strip[0].mono = 1
+    $vmr.strip[0].mono
+    $vmr.strip[0].mono = 0
+    $vmr.strip[0].mono
+
+    $vmr.bus[2].mute = 1
+    $vmr.bus[2].mute
+    $vmr.bus[2].mute = 0
+    $vmr.bus[2].mute
 }
 finally
 {
-    Logout
+    $vmr.Logout()
 }
