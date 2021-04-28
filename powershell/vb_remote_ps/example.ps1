8cb2a4705239c3f79da98378f3bc7b4bbd1023b1
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-04-28 14:41:45 +0100

    Create example.ps1
    Added simple example, will expand on this later

---
+++ powershell/vb_remote_ps/example.ps1
@@ -0,0 +1,15 @@
+. $PSScriptRoot\lib\voicemeeter.ps1
+
+Login
+
+$button[0].state = 1
+$button[0].state
+$button[0].state = 0
+$button[0].state
+
+$strip[0].mono = 1
+$strip[0].mono
+$strip[0].mono = 0
+$strip[0].mono
+
+Logout
