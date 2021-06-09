9bfc814379e672e5192f33b2d133c05ff46ab78c
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-06-09 18:28:49 +0100

    add pickrandomcards
    Add pickrandomcards.

---
+++ cs/PickRandomCards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of cards to pick: ");
+
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                /*
+                    iterate through the string array returned by PickSomeCards()
+                */
+                foreach(string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: Please enter an integer value");
+            }
+            
+        }
+    }
+}
