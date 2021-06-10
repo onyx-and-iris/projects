418c18258895e64d199394c3eb869f28cf38e0e5
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-06-10 18:46:19 +0100

    add picksomecardsUI
    Add picksomecardsUI.
    
    reusing cardpicker.cs class from cardpicker console app.
    
    define UI in xaml and control behaviour in xaml.cs

---
+++ cs/PickSomeCardsUI/CardPicker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickACardUI
+{
+    class CardPicker
+    {
+        // initialize random number generator
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            // generate random number from 1 to 4
+            int value = random.Next(1, 5);
+
+            // return suit according to random int
+            switch (value)
+            {
+                case 1:
+                    return "Spades";
+                case 2:
+                    return "Hearts";
+                case 3:
+                    return "Clubs";
+                default:
+                    return "Diamonds";
+            }
+        }
+
+        private static string RandomValue()
+        {
+            // generate random number from 1 to 13
+            int value = random.Next(1, 14);
+
+            // return card according to random int
+            switch (value)
+            {
+                case 1:
+                    return "Ace";
+                case 11:
+                    return "Jack";
+                case 12:
+                    return "Queen";
+                case 13:
+                    return "King";
+                default:
+                    return value.ToString();
+            }
+        }
+    }
+}
