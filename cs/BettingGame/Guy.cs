050642d5c16bd1d075076e01e683bbe698c08a19
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-06-11 14:13:13 +0100

    add bettingGame
    Using the same Guy class as simpleTransaction.
    
    Program populates Guy array and runs a series of double or nothing bets with predefined odds and ends if 'q' entered or no remaining players.

---
+++ cs/BettingGame/Guy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Betting
+{
+    class Guy
+    {
+        public Guy()
+        {
+        }
+
+        public string name { get; set; }
+        public int cash { get; set; }
+
+        /// <summary>
+        /// Write the transaction summary to console. Included info:
+        /// Give/receiver of cash.
+        /// Amount each person has after transaction.
+        /// Error if transaction was not valid.
+        /// </summary>
+        /// <param name="giver">The person who gave the cash</param>
+        /// <param name="amountTransferred">The amount of cash transferred</param>
+        public void Summary(string giver, int amountTransferred)
+        {
+            Console.WriteLine(
+                String.Format("{0} just {1} {2} ", name, giver == name ? "gave" : "accepted", amountTransferred)
+                );
+            Console.WriteLine("{0} now has {1} bucks", name, cash);
+        }
+
+        /// <summary>
+        /// Validation method to ensure amount given isn't negative.
+        /// </summary>
+        /// <param name="amount">
+        /// amount of cash given.
+        /// </param>
+        /// <returns>
+        /// amount subtracted from wallet or 0 if invalid number.
+        /// </returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(String.Format("Error: {0} cannot hand a negative amount!", name));
+                return 0;
+            }
+            else if (amount > cash)
+            {
+                Console.WriteLine(String.Format("Error: That is more than {0} can give!", name));
+                return 0;
+            }
+            cash -= amount;
+            return amount;
+        }
+
+        /// <summary>
+        /// Validation method to ensure amount received isn't negative.
+        /// </summary>
+        /// <param name="amount">
+        /// amount added to wallet.
+        /// </param>
+        public void TakeCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(String.Format("Error: {0} will not accept a negative amount!", name));
+            }
+            else
+            {
+                cash += amount;
+            }
+        }
+    }
+}
---
+++ cs/BettingGame/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Betting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MAX_PLAYERS = 3;
+            double odds = 0.75;
+            SortedList<int, string> playerList = new SortedList<int, string>()
+            {
+                { 0, "Bob" },
+                { 1, "Bill" },
+                { 2, "Joe" },
+            };
+            Guy[] players = InitializeArray<Guy>(MAX_PLAYERS);
+
+            for ( int i = 0; i < MAX_PLAYERS; ++i )
+            {
+                players[i] = new Guy() { name = playerList[i], cash = 100 };
+            }
+
+            Console.WriteLine(String.Format("Welcome to the casino. The odds are {0}", odds));
+            startBets(MAX_PLAYERS, odds, players);
+        }
+
+        /// <summary>
+        /// Helper method:
+        /// Initialize an array of type T objects as well as each element.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Placeholder for array of type T.
+        /// </typeparam>
+        /// <param name="length">
+        /// Number of elements in the array.
+        /// </param>
+        /// <returns>
+        /// An array of type T
+        /// </returns>
+        static T[] InitializeArray<T>(int length) where T : new()
+        {
+            T[] array = new T[length];
+            for (int i = 0; i < length; ++i)
+            {
+                array[i] = new T();
+            }
+
+            return array;
+        }
+
+        static void startBets(int MAX_PLAYERS, double odds, Guy[] players)
+        {
+            Random random = new Random();
+            int playersRemaining = MAX_PLAYERS;
+
+            while (playersRemaining > 0)
+            {
+                for (int i = 0; i < MAX_PLAYERS; ++i)
+                {
+                    if (players[i].cash > 0) {
+                        Console.WriteLine(String.Format("Player {0} has {1} bucks left to bet", players[i].name, players[i].cash));
+                        Console.Write(String.Format("How much would {0} like to bet? (or 'q' to quit) ", players[i].name));
+                        string amount = Console.ReadLine();
+                        if (amount == "q") return;
+
+                        if (int.TryParse(amount, out int result))
+                        {
+                            int pot = 2 * result;
+
+                            if (random.NextDouble() > odds)
+                            {
+                                players[i].TakeCash(pot);
+                            }
+                            else
+                            {
+                                players[i].GiveCash(result);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, please enter numeric value");
+                        }
+
+                        if (players[i].cash == 0)
+                        {
+                            Console.WriteLine("Sorry, you're out of luck today, see you next time!");
+                            --playersRemaining;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
