feebe1111fd8e663852b3f2a8271c62e93a92bce
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-06-18 12:00:06 +0100

    add lab 2 and 3
    add lab 2 and 3
    
    generates one balls at random locations and keeps a track of its number.

---
+++ cs/Lab 2 and 3/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject OneBallPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("AddABall", 1.5F, 1);
+    }
+
+    void AddABall()
+    {
+        Instantiate(OneBallPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ cs/Lab 2 and 3/OneBallBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneBallBehaviour : MonoBehaviour
+{
+    public float XRotation = 0;
+    public float YRotation = 1;
+    public float ZRotation = 0;
+    public float DegreesPerSecond = 180;
+    public int BallNumber = 0;
+
+    static int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(3 - Random.value * 6,
+            3 - Random.value * 6, 3 - Random.value * 6);
+        BallNumber = ++count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
+    }
+}
