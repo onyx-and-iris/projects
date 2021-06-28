a6ecf42ecf67c5b5f1429992737bb17d4256a274
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-06-28 15:59:13 +0100

    Add randomMenuItem
    Added randomMenuItem

---
+++ cs/RandomMenu/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MakeTheMenu();
+        }
+        /// <summary>
+        /// Generate a random menu, for items 3 and 4 initialize bagel field.
+        /// </summary>
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < 3)
+                {
+                    menuItems[i] = new MenuItem();
+                } else {
+                    menuItems[i] = new MenuItem()
+                    {
+                        Breads = new string[] { "plain bagel", "onion bagel",
+                            "pumpernickel bagel", "everything bagel" }
+                    };
+                }
+            }
+
+            item1.Text = menuItems[0].GenerateMenuItem();
+            price1.Text = menuItems[0].RandomPrice();
+            item2.Text = menuItems[0].GenerateMenuItem();
+            price2.Text = menuItems[0].RandomPrice();
+            item3.Text = menuItems[0].GenerateMenuItem();
+            price3.Text = menuItems[0].RandomPrice();
+            item4.Text = menuItems[0].GenerateMenuItem();
+            price4.Text = menuItems[0].RandomPrice();
+            item5.Text = menuItems[0].GenerateMenuItem();
+            price5.Text = menuItems[0].RandomPrice();
+
+            MenuItem specialMenuItem = new MenuItem()
+            {
+                Proteins = new string[] { "Organic ham", "Mushroom patty", "Mortadella" },
+                Breads = new string[] { "a gluten free roll", "a wrap", "pita" },
+                Condiments = new string[] { "dijon mustard", "miso dressing", "au jus" }
+            };
+
+            item6.Text = specialMenuItem.GenerateMenuItem();
+            price6.Text = specialMenuItem.RandomPrice();
+
+            guacomole.Text = String.Format("Add guacomole for {0}", specialMenuItem.RandomPrice());
+        }
+    }
+}
---
+++ cs/RandomMenu/MenuItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomMenu
+{
+    class MenuItem
+    {
+        public static Random Randomizer = new Random();
+
+        public string[] Proteins = { "Roast beef", "Salami", "Turkey",
+        "Ham", "Pastrami", "Tofu" };
+        public string[] Condiments = { "yellow mustard", "brown mustard",
+        "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] Breads = { "rye", "white", "wheat", "pumpernickel", "a roll" };
+
+        public string GenerateMenuItem()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            return string.Format("{0} with {1} on {2}", randomProtein, randomCondiment, randomBread);
+        }
+
+        public string RandomPrice()
+        {
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks = (cents * 0.1M);
+            return price.ToString("c");
+        }
+    }
+}
