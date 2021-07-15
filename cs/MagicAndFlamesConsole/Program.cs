8346f1a164d704f1b98319ce4c1c1fa45cadefa9
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2021-07-15 16:49:54 +0100

    add magicandflamesconsole
    console app version

---
+++ cs/MagicAndFlamesConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MagicAndFlames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            SwordDamage swordDamage = new SwordDamage();
+
+            while (true)
+            {
+                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, " +
+                    "3 for both, anything else to quit: ");
+
+                char key = Console.ReadKey().KeyChar;
+
+                char[] opts = { '0', '1', '2', '3' };
+                if (!Array.Exists(opts, x => x == key)) return;
+
+                int roll = 0;
+                for (int i = 0; i < 3; ++i)
+                {
+                    roll += random.Next(1, 7);
+                }
+
+                swordDamage.Roll = roll;
+                swordDamage.SetMagic(key == '1' || key == '3');
+                swordDamage.SetFlaming(key == '2' || key == '3');
+
+                Console.WriteLine(String.Format("\nRolled {0} for {1} HP\n", roll, swordDamage.Damage));
+            }
+        }
+    }
+}
